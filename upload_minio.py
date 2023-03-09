27f9e2cb5b2d4856e6a9e023193de5da0ae22681
yance <1415940604@qq.com>
2023-03-09 10:56:27 +0800

    fix

--- upload_minio.py
+++ upload_minio.py
@@ -16,5 +16,15 @@ def upload_directory(directory, bucket_name, minioClient):
             res = minioClient.fput_object(bucket_name, object_name, file_path)
 
 
+# delete directory 'test' from Minio bucket 'my-bucketname'
+def deleteFolder(bucketname, folderName):
+    # Delete using "remove_object"
+    objects_to_delete = minioClient.list_objects(
+        bucketname, prefix=folderName, recursive=True)
+    for obj in objects_to_delete:
+        minioClient.remove_object(bucketname, obj.object_name)
+
+deleteFolder('labelfree', 'site')
+
 # Upload directory 'test' to Minio bucket 'my-bucketname'
-upload_directory('site', 'asiatrip', minioClient)
+upload_directory('site', 'labelfree', minioClient)
