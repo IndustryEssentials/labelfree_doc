d9981ad5525758ba80f758405f580482fe551f2e
yance <1415940604@qq.com>
2023-03-09 13:43:26 +0800

    fix

--- upload_minio.py
+++ upload_minio.py
@@ -2,7 +2,7 @@ from minio import Minio
 import os
 import glob
 import mimetypes
-
+import filetype 
 # Initialize minioClient with an endpoint and access/secret keys.
 minioClient = Minio('192.168.12.137:9000',
                     access_key='labelfree',
@@ -20,9 +20,9 @@ def upload_directory(directory, bucket_name, minioClient):
             #     file_stat = os.stat(file_path)
             # minioClient.put_object(
             #             bucket_name, file_path, file_data, file_stat.st_size)
-            content_type = mimetypes.guess_type(file_path)[1]
+            kind = filetype.guess(file_path)
             res = minioClient.fput_object(
-                bucket_name, f"{file_path}", file_path, content_type=content_type)
+                bucket_name, f"{file_path}", file_path, content_type=kind.mime)
               
 
 
